39e4b08803f15e287f4ae5b62b7b32f8774149b5
Janeiro Cathey <janeiro.cathey@frendorsed.com>
2013-11-14 12:25:15 -0800

    Implemented Devise for Users

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,8 @@ Pinteresting::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+  
+  #note to set to your actual host
+  #required for Heroku
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 end
